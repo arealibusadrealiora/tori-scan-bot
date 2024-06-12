a37a00b27b64434abaa844e8182e6135786b0986
a realibus, ad realiora <113445322+arealibusadrealiora@users.noreply.github.com>
2024-06-12 22:45:03 +0300

    Moved load functions to a separate module

---
+++ loaders.py
@@ -0,0 +1,39 @@
+import json
+
+def load_categories(language: str) -> dict:
+    '''
+    Load category data from a JSON file based on the specified language.
+    Args:
+        language (str): Language code.
+    Returns:
+        dict: Category data.
+    '''
+    with open(f'jsons/categories/{language}.json', encoding='utf-8') as f:
+        categories_data = json.load(f)
+    return categories_data
+
+
+def load_locations(language: str) -> dict:
+    '''
+    Load location data from a JSON file based on the specified language.
+    Args:
+        language (str): Language code.
+    Returns:
+        dict: Location data.
+    '''
+    with open(f'jsons/locations/{language}.json', encoding='utf-8') as f:
+        locations_data = json.load(f)
+    return locations_data
+
+
+def load_messages(language: str) -> dict:
+    '''
+    Load message templates from a JSON file based on the specified language.
+    Args:
+        language (str): Language code.
+    Returns:
+        dict: Message templates.
+    '''
+    with open(f'jsons/messages/{language}.json', encoding='utf-8') as f:
+        messages_data = json.load(f)
+    return messages_data
