2c8fc0ad24b06557708fe1846041b4febc4eff27
a realibus, ad realiora <113445322+arealibusadrealiora@users.noreply.github.com>
2024-09-26 16:23:22 +0300

    Update jobs.py

--- modules/jobs.py
+++ modules/jobs.py
@@ -71,5 +71,7 @@ def setup_jobs(job_queue):
     Args:
         job_queue: The job queue to which the job should be added.
     '''
+    async def wrapper(context):
+        await check_for_new_items(context)
     # interval is in seconds; 300 seconds = 5 minutes; please don't put it lower than thst, it's pointless.
     job_queue.run_repeating(check_for_new_items, interval=300, first=0)
