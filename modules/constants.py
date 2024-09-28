cacbfe37686cf83487bb0bb085ef13f6e775115c
a realibus, ad realiora <113445322+arealibusadrealiora@users.noreply.github.com>
2024-09-28 15:58:40 +0300

    Update constants.py

--- modules/constants.py
+++ modules/constants.py
@@ -1,10 +1,35 @@
 # Conversation states
-LANGUAGE, ITEM, CATEGORY, SUBCATEGORY, PRODUCT_CATEGORY, REGION, CITY, AREA, MAIN_MENU, SETTINGS_MENU, CONFIRMATION = range(11)
+(LANGUAGE, ITEM, CATEGORY, SUBCATEGORY, 
+ PRODUCT_CATEGORY, REGION, CITY, AREA, 
+ MAIN_MENU, SETTINGS_MENU, CONFIRMATION) = range(11)
 
-# Other constants
-ALL_CATEGORIES = ['kaikki kategoriat', 'all categories', 'все категории', 'всі категорії']
-ALL_SUBCATEGORIES = ['kaikki alaluokat', 'all subcategories', 'все подкатегории', 'всі підкатегорії']
-ALL_PRODUCT_CATEGORIES = ['kaikki tuoteluokat', 'all product categories', 'все категории товаров', 'всі категорії товарів']
-WHOLE_FINLAND = ['koko suomi', 'whole finland', 'вся финляндия', 'вся фінляндія']
-ALL_CITIES = ['kaikki kaupungit', 'all cities', 'все города', 'всі міста']
-ALL_AREAS = ['kaikki alueet', 'all areas', 'все области', 'всі райони']
+# Language constants
+ALL_CATEGORIES          = (['kaikki kategoriat',
+                            'all categories',
+                            'все категории',
+                            'всі категорії'])
+
+ALL_SUBCATEGORIES       = (['kaikki alaluokat',
+                            'all subcategories',
+                            'все подкатегории',
+                            'всі підкатегорії'])
+
+ALL_PRODUCT_CATEGORIES  = (['kaikki tuoteluokat',
+                           'all product categories',
+                           'все категории товаров',
+                           'всі категорії товарів'])
+
+WHOLE_FINLAND           = (['koko suomi',
+                            'whole finland',
+                            'вся финляндия',
+                            'вся фінляндія'])
+
+ALL_CITIES              = (['kaikki kaupungit',
+                            'all cities',
+                            'все города',
+                            'всі міста'])
+
+ALL_AREAS               = (['kaikki alueet',
+                            'all areas',
+                            'все области',
+                            'всі райони'])
